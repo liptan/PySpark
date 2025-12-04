3bbc234239738e4956ffe482af7d68476cdce7af
liptan <38709699+liptan@users.noreply.github.com>
2025-12-04 15:11:36 +0800

    Create sparkSessionTest.py
    SparkSession unit Test

---
+++ sparkSessionTest.py
@@ -0,0 +1,11 @@
+import pytest
+from pyspark.sql import SparkSession
+
+@pytest.fixture(scope="session")
+def spark():
+    spark = SparkSession.builder \
+        .appName("pytest-pyspark-local-testing") \
+        .master("local[*]") \
+        .getOrCreate()
+    yield spark
+    spark.stop()
